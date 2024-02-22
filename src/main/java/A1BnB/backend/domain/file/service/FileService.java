8859d6188727d1de5c13a03d69040d13d7006bb9
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-02-22 19:11:37 +0900

    feat: S3Service 작성

--- src/main/java/A1BnB/backend/domain/file/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package A1BnB.backend.domain.file.service;
-
-import java.io.IOException;
-import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    List<String> uploadPhoto(List<MultipartFile> photos, List<String> photoNames) throws IOException;
-}
