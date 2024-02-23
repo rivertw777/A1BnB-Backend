43485adc7e717d113f3f2eab895b3bef47559411
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-02-23 22:53:06 +0900

    refactor: logAfter 메서드 수정

--- src/main/java/A1BnB/backend/global/aop/LoggingAspect.java
+++ src/main/java/A1BnB/backend/global/aop/LoggingAspect.java
@@ -1,8 +1,9 @@
 package A1BnB.backend.global.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -16,7 +17,7 @@ public class LoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     // 예외 발생 시 로깅
-    @AfterThrowing(pointcut = "execution(* A1BnB.backend.domain.*.service.*.*(..))", throwing = "ex")
+    @AfterThrowing(pointcut = "execution(* A1BnB.backend.*.*.service.*.*(..))", throwing = "ex")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getSignature().getDeclaringTypeName();
@@ -25,7 +26,7 @@ public class LoggingAspect {
     }
 
     // 메서드 실행 이전 로깅
-    @Before("execution(* A1BnB.backend.domain.*.service.*.*(..))")
+    @Before("execution(* A1BnB.backend.*.*.service.*.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getSignature().getDeclaringTypeName();
@@ -34,18 +35,28 @@ public class LoggingAspect {
     }
 
     // 메서드 실행 이후 로깅
-    @After("execution(* A1BnB.backend.domain.*.service.*.*(..))")
-    public void logAfter(JoinPoint joinPoint) {
+    @Around("execution(* A1BnB.backend.*.*.service.*.*(..)) || ")
+    public Object logAfter(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String layer = getLayerName(className);
-        logger.info("[{}] [Completed] {}.{}()", layer, className, methodName);
+
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        logger.info("[{}] [Completed] {}.{}() - Execution Time: {}ms", layer, className, methodName, executionTime);
+        return result;
     }
+
 
     // 클래스 이름으로부터 계층(layer) 이름 추출
     private String getLayerName(String className) {
         if (className.contains("service")) {
             return "Service";
+        } else if (className.contains("controller")) {
+            return "Controller";
         }
         return "Unknown";
     }
