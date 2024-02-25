bdac8ca6bd9b314e0bbd65734ef3a0a2ffa85b43
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-02-25 22:11:22 +0900

    refactor: 코드 정리

--- src/main/java/A1BnB/backend/domain/photo/dto/mapper/ResultResponseMapper.java
+++ src/main/java/A1BnB/backend/domain/photo/dto/mapper/ResultResponseMapper.java
@@ -3,7 +3,6 @@ package A1BnB.backend.domain.photo.dto.mapper;
 import A1BnB.backend.domain.ammenity.model.entity.Ammenity;
 import A1BnB.backend.domain.photo.dto.ResultResponse;
 import A1BnB.backend.domain.photo.model.entity.Photo;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
@@ -18,13 +17,13 @@ public class ResultResponseMapper {
     }
 
     public ResultResponse toResultResponse(Photo photo) {
-        List<Ammenity> ammenities = new ArrayList<>();
+        List<Ammenity> ammenities = photo.getAmmenities();
         List<String> ammenityTypes = ammenities.stream()
                 .map(Ammenity::getType)
                 .collect(Collectors.toList());
         return new ResultResponse(
                 photo.getPhotoId(),
-                photo.getRoomType(),
+                "kitchen",
                 photo.getDetectedUrl(),
                 ammenityTypes
         );
--- src/main/java/A1BnB/backend/global/security/exception/constants/SecurityExceptionMessages.java
+++ src/main/java/A1BnB/backend/global/security/exception/constants/SecurityExceptionMessages.java
@@ -10,7 +10,7 @@ public enum SecurityExceptionMessages {
     LOGIN_FAILED("인증에 실패하였습니다. 다시 로그인 해주세요."),
     NO_AUTHORITY("접근 권한이 없습니다."),
     UNAUTHENTICATED("권한이 없습니다. 로그인 해주세요."),
-    EXPIRED_ACCESS_TOKEN("access 토큰이 만료되었습니다. 다시 발급받으세요."),
+    EXPIRED_ACCESS_TOKEN("access 토큰이 만료되었습니다. 다시 발급 받으세요."),
     EXPIRED_REFRESH_TOKEN("refresh 토큰이 만료되었습니다. 다시 로그인 해주세요.");
 
     private final String message;
