b5a176a30c1c82223794d233fd375db2bb559b40
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-03-12 23:33:35 +0900

    test: 코드 삭제

--- src/test/java/A1BnB/backend/global/redis/service/RedisServiceTest.java
+++ src/test/java/A1BnB/backend/global/redis/service/RedisServiceTest.java
@@ -11,3 +11,56 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+class RedisServiceTest {
+    final String KEY = "username";
+    final String VALUE = "refreshToken";
+    final Long DURATION = 6000L;
+
+    @Autowired
+    private RedisService redisService;
+
+    @BeforeEach
+    void setUp() {
+        redisService.setRefreshToken(KEY, VALUE, DURATION);
+    }
+
+    @AfterEach
+    void shutDown() {
+        redisService.deleteRefreshToken(KEY);
+    }
+
+    @Test
+    @DisplayName("Refresh 토큰 조회 확인")
+    void saveAndFindTest() {
+        // when
+        String findToken = redisService.getRefreshToken(KEY);
+        // then
+        assertThat(findToken).isEqualTo(VALUE);
+    }
+
+    @Test
+    @DisplayName("Refresh 토큰 삭제 확인")
+    void deleteTest() {
+        // when
+        redisService.deleteRefreshToken(KEY);
+        String findValue = redisService.getRefreshToken(KEY);
+        // then
+        assertThat(findValue).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("만료시간 이후 토큰 삭제 확인")
+    void expiredTest() {
+        // when
+        String findValue = redisService.getRefreshToken(KEY);
+        await().pollDelay(Duration.ofMillis(6000L)).untilAsserted(
+                () -> {
+                    String expiredValue = redisService.getRefreshToken(KEY);
+                    assertThat(expiredValue).isNotEqualTo(findValue);
+                    assertThat(expiredValue).isEqualTo(null);
+                }
+        );
+    }
+
+}
