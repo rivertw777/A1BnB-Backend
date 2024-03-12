a02761bbe2f2d43cc23cfe5d26ea40c977acfb65
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-03-13 02:03:44 +0900

    test: testcontainer 제외

--- src/test/java/A1BnB/backend/global/redis/service/RedisServiceTest.java
+++ src/test/java/A1BnB/backend/global/redis/service/RedisServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@ExtendWith(RedisTestContainerConfig.class)
+//@ExtendWith(RedisTestContainerConfig.class)
 @SpringBootTest
 class RedisServiceTest {
 
