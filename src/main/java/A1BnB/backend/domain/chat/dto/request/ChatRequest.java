c2544b9fbda1cb5b27a66fe26bcdf5792a8092f1
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-03-23 23:00:01 +0900

    refactor: Chat - ChatRequest 수정

--- src/main/java/A1BnB/backend/domain/chat/dto/request/ChatRequest.java
+++ src/main/java/A1BnB/backend/domain/chat/dto/request/ChatRequest.java
@@ -2,7 +2,7 @@ package A1BnB.backend.domain.chat.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 
-public record ChatRequest(@NotNull Long roomId, @NotNull String message) {
+public record ChatRequest(@NotNull Long roomId, @NotNull String message, @NotNull String receiverName) {
     public ChatRequest {
     }
 }
