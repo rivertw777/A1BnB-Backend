42135594b2381bdace6ed07168ae89acb82fc854
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-04-28 20:50:25 +0900

    refactor: swagger 설정 삭제

--- src/main/java/A1BnB/backend/global/security/config/SecurityConfig.java
+++ src/main/java/A1BnB/backend/global/security/config/SecurityConfig.java
@@ -62,8 +62,6 @@ public class SecurityConfig {
                         .requestMatchers(antMatcher(HttpMethod.POST, "/api/posts/search")).permitAll()
                         // 게시물 인기순 조회
                         .requestMatchers(antMatcher(HttpMethod.POST, "/api/posts/like")).permitAll()
-                        // Swagger
-                        .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
                         // 웹소켓
                         .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
